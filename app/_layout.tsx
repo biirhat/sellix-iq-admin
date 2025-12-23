618398dd9222dcd00e8578cfb7ceb1c12f2113f1
Biirhat <biirhatmohamed@gmail.com>
2025-12-23 11:49:19 +0300

    chore: remove gluestack UI and generated components

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,10 +8,10 @@ import { StatusBar } from "expo-status-bar";
 import { HeroUINativeProvider } from "heroui-native";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import "react-native-reanimated";
+import { View } from 'react-native';
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
 
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import "@/global.css";
 
 export const unstable_settings = {
@@ -20,14 +20,13 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const theme = colorScheme === "dark" ? DarkTheme : DefaultTheme;
 
   return (
-    <GluestackUIProvider >
-      <GestureHandlerRootView  style={{ flex: 1 }}>
-        <HeroUINativeProvider>
-          <ThemeProvider
-            value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-          >
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <HeroUINativeProvider>
+        <ThemeProvider value={theme}>
+          <View style={{ flex: 1, backgroundColor: theme.colors.background }}>
             <Stack screenOptions={{ headerShown: false }}>
               <Stack.Screen name="(app)" options={{ headerShown: false }} />
               <Stack.Screen name="index" options={{ headerShown: false }} />
@@ -37,10 +36,10 @@ export default function RootLayout() {
                 options={{ presentation: "modal", title: "Modal" }}
               />
             </Stack>
-            <StatusBar style="auto" />
-          </ThemeProvider>
-        </HeroUINativeProvider>
-      </GestureHandlerRootView>
-    </GluestackUIProvider>
+            <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+          </View>
+        </ThemeProvider>
+      </HeroUINativeProvider>
+    </GestureHandlerRootView>
   );
 }
